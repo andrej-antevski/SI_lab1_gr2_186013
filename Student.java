8689469c7b611e1b7dd75085bea3be185ee10b57
Anto <andrej.antevski99@gmail.com>
2020-03-24 21:34:06 +0100

    students class finished

--- Student.java
+++ Student.java
@@ -3,15 +3,55 @@ class Student {
 	String firstName;
 	String lastName;
 	List<Integer> grades;
-	//TODO constructor
 
-	//TODO seters & getters
+	public Student(String index, String firstName, String lastName, List<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public List<Integer> getGrades() {
+		return grades;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGrades(List<Integer> grades) {
+		this.grades = grades;
+	}
 
 	public double getAverage() {
-		//TODO
+		int sum = 0;
+		for (Integer g:grades) {
+			sum+=g;
+		}
+		return (double) sum/grades.size();
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		return grades.size()*6;
 	}
 }
